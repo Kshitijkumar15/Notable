695afa15865b6576294a75c369d784e76f09db5d
Kshitij kumar singh <kshitijkumarsingh58@gmail.com>
2023-08-01 19:58:44 +0530

    Third commit
    Firebase authentication done

--- app/src/main/java/com/example/notepad/signup.java
+++ app/src/main/java/com/example/notepad/signup.java
@@ -1,5 +1,6 @@
 package com.example.notepad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,18 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class signup extends AppCompatActivity {
 
     private EditText msignupemail, msignuppassword;
     private RelativeLayout msignup;
     private TextView mgotologin;
-
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,7 @@ public class signup extends AppCompatActivity {
         msignuppassword = findViewById(R.id.signuppassword);
         msignup = findViewById(R.id.signup);
         mgotologin = findViewById(R.id.gotologin);
-
+        firebaseAuth = FirebaseAuth.getInstance();
 
         mgotologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,10 +55,40 @@ public class signup extends AppCompatActivity {
                 } else if (password.length() < 6) {
                     Toast.makeText(getApplicationContext(), "Password should be more than 6", Toast.LENGTH_SHORT).show();
                 } else {
-                    //registration
+                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Registration Done", Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Failed To Register", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
 
+    }
+
+    //Send email verification
+    private void sendEmailVerification(){
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null){
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "Verification Email Send", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(signup.this,MainActivity.class));
+                }
+            });
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Error Occured", Toast.LENGTH_SHORT).show();
+        }
     }
 }
